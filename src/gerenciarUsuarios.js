0a7a9e0bba133721136efde5243c23323e4e7e28
isabella Henriques <isabella.henriques@hotmail.com>
2025-09-11 07:24:14 -0300

    Mudei a lista de nomes para se tornarem objetos

--- src/gerenciarUsuarios.js
+++ src/gerenciarUsuarios.js
@@ -1,11 +1,36 @@
-const usuarios = ['Mathues', 'Marcos','Lucas', 'João']; // lista para armazenar os nomes
+// const usuarios = ['Mathues', 'Marcos','Lucas', 'João']; // lista para armazenar os nomes
+
+const usuarios = [
+    {
+        nome: 'Mathues',
+        email:'matheus@email.com'
+    },
+    {
+        nome: 'Marcos',
+        email:'marcos@email.com'
+    },
+    {
+        nome: 'Lucas',
+        email:'lucas@email.com'
+    },
+    {
+        nome: 'João',
+        email:'joão@email.com'
+    }    
+]; 
 
 function retornarUsuarios () {    
     return usuarios
 }
 
-function adicionarUsuario (nome) {
-    usuarios.push(nome); // adiciona o nome na lista usuarios
+// adiciona o usuário na lista usuarios
+function adicionarUsuario (usuario) {
+    usuarios.push(usuario); 
+}
+
+
+function capturarPropriedadeDoUsuario (indicDoUsuario, propriedade) {   
+    return
 }
 
 module.exports = { 
--- test/gerenciarUsuarios.test.js
+++ test/gerenciarUsuarios.test.js
@@ -3,9 +3,19 @@ const { expect } = require('chai');
 
 describe('Testar as funções de Gestão de Usuários', function () {
     it('Validar que posso adicionar um novo nome de usuário na lista', function () {
-        adicionarUsuario('Ana'); // adiciona o nome 'Ana' na lista
+       // Adiciona o nome 'Ana' na lista
+        adicionarUsuario({
+        nome: 'Ana',
+        email:'ana@email.com'
+    }); 
+
         const listaDeUsuarios = retornarUsuarios(); // retorna a lista de usuáriosnp
-        expect(listaDeUsuarios.at(-1)).to.equal('Ana'); // valida se o nome 'Ana' está na lista
+        //expect(listaDeUsuarios.at(-1).nome).to.equal('Ana'); // valida se o nome 'Ana' está na lista
+        //expect(listaDeUsuarios.at(-1).email).to.equal('ana@email.com'); // valida o email de 'Ana'da na lista
+        expect(listaDeUsuarios.at(-1)).to.eql({
+            nome: 'Ana',
+            email:'ana@email.com'
+        });
     });
 });
 
